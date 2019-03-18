bfb8a2ca45eba7a30c5c1ce41a61394860292ebc
Ryuichi Umehara <ryuichi-astrona@ymobile.ne.jp>
2019-03-18 18:37:19 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+#984b9e22025bc6123fc89f00f49cbc536169ad74　apitoken
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
